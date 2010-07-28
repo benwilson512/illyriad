ef8e3f8595c72dd3010ecb8a772755b023d1b7f2
ben <ben@infoether.com>
2010-07-28 14:48:20 -0400

    time to break things

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Illyriad::Application.routes.draw do |map|
-
-  # devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
-  #  match 'login', :to => 'devise/sessions#new', :as => "new_user_session"
-  #  match 'logout', :to  => 'devise/sessions#destroy', :as => "destroy_user_session"
-  #  match 'signup', :to => 'devise/registrations#new', :as => "new_user_registration"
-
+  
+  devise_for :users
+  
   resources :players, :collection => [:find]
   resources :alliances
   resources :towns do
