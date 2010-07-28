129afe0deb0096faa11f0cd46b6fb5fcf12c0b75
ben <ben@infoether.com>
2010-07-28 15:26:06 -0400

    closing the doors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   
+  before_filter :authenticate_user!
+  
   private
   def data_cache(key)
     unless output = Rails.cache.read(key)
