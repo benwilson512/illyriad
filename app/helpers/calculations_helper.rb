94fd2241f7fc3ac4ef07efd384e819042f313c2b
Ben Wilson <benwilson512@gmail.com>
2010-09-08 11:51:50 -0400

    test

--- app/helpers/calculations_helper.rb
+++ app/helpers/calculations_helper.rb
@@ -29,4 +29,29 @@ module CalculationsHelper
     code << "[/table]"
   end
   
+  def get_html(report_items, *speed)
+    code = "<h1>Target</h1><h2>#{@town.name}</h2><p>Population #{@town.population} </p><p>#{@report_items.size} valid results for town: <b>#{@town.name}</b></p>"
+    code << "<table>
+        <tr>
+          <th>Town Name</th>
+          <th>Town Owner</th>
+          <th>Population</th>
+          <th>Alliance</th>
+          <th>Time (speed #{speed})</th>
+          <th>Siege Time</th>
+        </tr>"
+    @report_items.each do |report_item|
+    code << "<tr>
+          <td>#{link_to report_item[:town].name, town_path(report_item[:town])}</td>
+          <td>#{link_to report_item[:town].player.name, player_path(report_item[:town].player)}</td>
+          <td>#{report_item[:town].population}</td>
+          <td>#{link_to_alliance(report_item[:town].player)}</td>
+          <td>#{format_time(report_item[:time])}</td>
+          <td>#{format_siege_time report_item[:distance]}</td>
+    	  <td>#{link_to "view", "http://uk1.illyriad.co.uk/view_map.asp?Radius=5&CenterX=#{report_item[:town].x}&CenterY=#{report_item[:town].y}"}</td>
+        </tr>"
+        end
+    code << "</table>"
+  end
+  
 end
